cc267497828acaa68cf120324302657db1f755bb
DamianRincon <damianrincondrc@gmail.com>
2018-03-13 03:30:58 -0600

    Initial commit

---
+++ app/src/main/java/es/rincon/damian/comandarduino/DeviceList.java
@@ -0,0 +1,13 @@
+package es.rincon.damian.comandarduino;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeviceList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_list);
+    }
+}
